e2dd4b29120d70ac37da037b04be2f0508938ae3
Gaga <qq40124@gmail.com>
2020-07-02 00:44:03 +0800

    week02完成

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,11 @@
 function reverse(str) {
-  
+  console.log(str.split("").reverse().join(""));
+  /*
+	執行順序為：
+	1. split 分割字串 -> [h,e,l,l,o]
+	2. reverse 反轉顯示 ->[o,e,l,l,h]
+	3. join 合併字元 -> oellh
+  */
 }
 
 reverse('hello');
